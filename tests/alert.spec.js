c5dd6d25d0221971ece1b9dfdfb9d492629e7f7b
Gayathri-tech-coder <gayathri.softlogic@gmail.com>
2026-09-18 18:46:52 +0530

    Added Codes

--- tests/alert.spec.js
+++ tests/alert.spec.js
@@ -3,14 +3,14 @@ test.use({viewport:{width:1536, height:864}})
 test('Page1',async({page})=>{
     await page.goto('https://testautomationpractice.blogspot.com/');
     await page.mouse.wheel(0,500)
-   /* 
+   
     page.on('dialog',async dialog=>{
         await page.waitForTimeout(3000);//3 sec
         expect(dialog.type()).toContain('alert')
         expect(dialog.message()).toContain('I am an alert box!')
         await dialog.accept(); //ok
     })
-    await page.click('#alertBtn')*/
+    await page.click('#alertBtn')
 
     //Confirm Alert:
     /*    
@@ -24,16 +24,16 @@ test('Page1',async({page})=>{
     await expect(page.locator('#demo')).toHaveText('You pressed Cancel!')*/
 
     //Prompt Alert:
-    page.on('dialog',async dialog=>{
-        await page.waitForTimeout(3000);//3 sec
-        expect(dialog.type()).toContain('prompt')
-        expect(dialog.message()).toContain('Please enter your name:')
-        // await dialog.dismiss(); //cancel
-        expect(dialog.defaultValue()).toContain('Harry Potter')
-        await dialog.accept('John')
-    })
-    await page.click('#promptBtn')
-    await expect(page.locator('#demo')).toHaveText('Hello John! How are you today?')
+    // page.on('dialog',async dialog=>{
+    //     await page.waitForTimeout(3000);//3 sec
+    //     expect(dialog.type()).toContain('prompt')
+    //     expect(dialog.message()).toContain('Please enter your name:')
+    //     // await dialog.dismiss(); //cancel
+    //     expect(dialog.defaultValue()).toContain('Harry Potter')
+    //     await dialog.accept('John')
+    // })
+    // await page.click('#promptBtn')
+    // await expect(page.locator('#demo')).toHaveText('Hello John! How are you today?')
    
     await page.waitForTimeout(3000);//3 sec
 })
