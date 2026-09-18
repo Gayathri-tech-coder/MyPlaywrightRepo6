0f0a8f314c87a3ea1679b338e0b63218713eec06
Gayathri-tech-coder <gayathri.softlogic@gmail.com>
2026-09-18 18:54:29 +0530

    config

--- playwright.config.js
+++ playwright.config.js
@@ -32,10 +32,10 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'https://dummy.restapiexample.com/',
+    // baseURL: 'https://dummy.restapiexample.com/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'off',
+    trace: 'on',
     // screenshot:'only-on-failure',
     // video : 'retain-on-failure',
   },
